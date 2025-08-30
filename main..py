66ee79f8f1a5527db5d3d1330a077dccc91450ad
kuvsamja <136931050+kuvsamja@users.noreply.github.com>
2025-08-30 21:41:32 +0200

    added vec3 class

--- main..py
+++ main..py
@@ -4,7 +4,57 @@ image_width = 400
 aspect_ratio = 4 / 3
 image_height = int(image_width // aspect_ratio)
 
-def main():
+class vec3():
+    def __init__(self, x = 0, y = 0, z = 0):
+        self.e = [x, y, z]
+    def x(self):    return self.e[0]
+    def y(self):    return self.e[1]
+    def z(self):    return self.e[2]
+
+    def __repr__(self):
+        return f"({self.e[0], self.e[1], self.e[2]})"
+    
+    def __neg__(self):  return vec3(-self.e[0], -self.e[1], -self.e[2])
+
+    def __getitem__(self, i):  return self.e[i]
+    def __setitem__(self, i, value):    self.e[i] = value
+
+
+    def __iadd__(self, v):
+        self.e[0] += v.e[0]
+        self.e[1] += v.e[1]
+        self.e[2] += v.e[2]
+        return self
+    def __isub__(self, v):
+        self.e[0] -= v.e[0]
+        self.e[1] -= v.e[1]
+        self.e[2] -= v.e[2]
+        return self
+    
+    def __imul__(self, t):
+        self.e[0] *= t
+        self.e[1] *= t
+        self.e[2] *= t
+        return self
+    def __itruediv__(self, t):
+        self.e[0] /= t
+        self.e[1] /= t
+        self.e[2] /= t
+        return self
+    
+    def __add__(self, v):
+        return vec3(self.e[0] + v.e[0], self.e[1] + v.e[1], self.e[2] + v.e[2])
+    def __sub__(self, v):
+        return vec3(self.e[0] - v.e[0], self.e[1] - v.e[1], self.e[2] - v.e[2])
+    
+    def __mul__(self, t):
+        return vec3(self.e[0] * t, self.e[1] * t, self.e[2] * t)
+    def __rmul__(self, t):
+        return vec3(self.e[0] * t, self.e[1] * t, self.e[2] * t)
+    def __truediv__(self, t):
+        return vec3(self.e[0] / t, self.e[1] / t, self.e[2] / t)
+        
+def draw():
     with open("img.ppm", "w") as f:
         f.write(f"P3\n{image_width} {image_height}\n255\n")
         
@@ -13,7 +63,7 @@ def main():
                 sys.stdout.write(f"{i} {j} \n")
                 r = i / (image_width - 1)
                 g = j / (image_height - 1)
-                b = 0.0
+                b = 0
 
                 ir = int(255 * r)
                 ig = int(255 * g)
@@ -21,5 +71,9 @@ def main():
             
                 f.write(f"{str(ir)} {str(ig)} {str(ib)}\n")
     print("done")
+
+def main():
+    draw()
+
 if __name__ == "__main__":
     main()
